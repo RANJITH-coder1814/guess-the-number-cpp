fac95d07b63a209b57ab6e02288e48bc9d858c7f
Ranjith.HL <ranjithranjith2747@gmail.com>
2026-01-29 09:59:14 +0530

    Create guess-the-number.cpp

---
+++ guess-the-number.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(0));
+    int number = rand() % 100 + 1;
+    int guess, attempts = 0;
+
+    cout << "===== Number Guessing Game =====\n";
+    cout << "Guess a number between 1 and 100\n\n";
+
+    do {
+        cout << "Enter your guess: ";
+        cin >> guess;
+        attempts++;
+
+        if (guess > number)
+            cout << "Too high! Try again.\n";
+        else if (guess < number)
+            cout << "Too low! Try again.\n";
+        else
+            cout << "\n Correct! You guessed it in " 
+                 << attempts << " attempts.\n";
+
+    } while (guess != number);
+
+    return 0;
+}
